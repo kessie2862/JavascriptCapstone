150b9b2a3e1b62446a609a395ff5c7975c7989fb
kessie <kayp3477@gmail.com>
2023-05-29 10:37:05 +0000

    Bundle files using webpack

---
+++ dist/bundleb38c1dba33306ac665d9.js
@@ -0,0 +1,40 @@
+"use strict";
+(self["webpackChunkjavascriptcapstone"] = self["webpackChunkjavascriptcapstone"] || []).push([["bundle"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   add: () => (/* binding */ add)
+/* harmony export */ });
+/* harmony import */ var _bundle_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./bundle.css */ "./src/bundle.css");
+
+var add = function add(a, b) {
+  return a + b;
+};
+
+/***/ }),
+
+/***/ "./src/bundle.css":
+/*!************************!*\
+  !*** ./src/bundle.css ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=bundleb38c1dba33306ac665d9.js.map
